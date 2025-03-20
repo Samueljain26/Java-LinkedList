c686b21c8cd4f667cf6a5da4c1585dfa40968055
Samueljain26 <samueljain77@gmail.com>
2025-03-20 09:55:05 +0530

    [Samuel] ADD: program to manage student records using a singly linked list.

---
+++ StudentRecodManagement.java
@@ -0,0 +1,195 @@
+class Student{
+    int rollNumber,age;
+    String name;
+    char grade;
+
+    Student(int rollNumber,String name,char grade,int age){
+        this.rollNumber=rollNumber;
+        this.name=name;
+        this.grade=grade;
+        this.age=age;
+    }
+}
+
+class SingleLinkedList{
+    class Node{
+        Student data;
+        Node next;
+
+        Node(){
+            data=null;
+            next=null;
+        }
+
+        Node(Student data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    Node head,tail;
+    SingleLinkedList(){
+        head=null;
+        tail=null;
+    }
+
+    void insertAtHead(Student data){
+        if(head==null){
+            Node n=new Node(data);
+            head=n;
+            tail=n;
+        }
+        else{
+            Node n=new Node(data);
+            n.next=head;
+            head=n;
+        }
+    }
+
+    void insertAtTail(Student data){
+        if(head==null){
+            Node n=new Node(data);
+            head=n;
+            tail=n;
+        }
+        else{
+            Node n=new Node(data);
+            tail.next=n;
+            tail=n;
+        }
+
+    }
+
+    void insertAtPosition(int index,Student data){
+        Node n=new Node(data);
+        if(head==null){
+            head=n;
+            tail=n;
+        }
+        else{
+            Node temp=head;
+            int counter=0;
+            while(temp!=null && counter<index){
+                temp=temp.next;
+                counter++;
+            }
+            if(temp==null){
+                tail.next=n;
+                tail=n;
+            }
+            else if (temp==head){
+                n.next=head;
+                head=n;
+            }
+            else{
+                n.next=temp.next;
+                temp.next=n;
+            }
+        }
+    }
+
+    void printStudents(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data.name+" "+temp.data.rollNumber+" "+temp.data.age+" "+temp.data.grade);
+            temp=temp.next;
+        }
+    }
+
+    void DeleteByRollnumber(int rollNumber){
+        if(head.data.rollNumber==rollNumber){
+            head=head.next;
+            System.out.println("found student at the head of the list ");
+            return ;
+        }
+        Node temp=head ;
+        while(temp!=null && (temp.next!=null && temp.next.data.rollNumber!=rollNumber)){
+            temp=temp.next;
+        }
+        if(temp==null || temp.next==null){
+            System.out.println("No SingleLinkedList.Student with given roll number exist");
+        }
+        if(temp.next!=null) {
+            temp.next = temp.next.next;//deleting the node
+        }
+
+    }
+
+    void findStudent(int rollNumber){
+        Node temp=head;
+        while(temp!=null){
+            if(temp.data.rollNumber==rollNumber){
+                System.out.println("SingleLinkedList.Student found ");
+                return ;
+            }
+            temp=temp.next;
+        }
+        System.out.println("SingleLinkedList.Student not found");
+    }
+
+    void updateStudentGrades(char grade,int rollNumber){
+        Node temp=head;
+        while(temp!=null){
+            if(temp.data.rollNumber==rollNumber){
+                System.out.println("Updating student Grades ");
+                temp.data.grade=grade;
+                return ;
+            }
+            temp=temp.next;
+        }
+        System.out.println("SingleLinkedList.Student not found");
+    }
+}
+
+public class StudentRecodManagement {
+    public static void main(String[] args) {
+
+        SingleLinkedList list = new SingleLinkedList();
+
+        Student student1 = new Student(101, "Alice", 'A', 20);
+        Student student2 = new Student(102, "Bob", 'B', 21);
+        Student student3 = new Student(103, "Charlie", 'C', 22);
+        Student student4 = new Student(104, "David", 'A', 20);
+        Student student5 = new Student(105, "Eve", 'B', 21);
+
+
+        list.insertAtPosition(1, student2);
+        list.insertAtPosition(0, student4);
+        list.insertAtPosition(3, student5);
+
+        list.insertAtHead(student1);
+        list.insertAtTail(student3);
+
+        list.printStudents();
+
+        list.DeleteByRollnumber(106);
+
+        list.printStudents();
+
+        list.findStudent(101);
+
+        list.updateStudentGrades('F',103);
+
+        list.printStudents();
+
+    }
+}
+/*Alice 101 20 A
+David 104 20 A
+Bob 102 21 B
+Eve 105 21 B
+Charlie 103 22 C
+No SingleLinkedList.Student with given roll number exist
+Alice 101 20 A
+David 104 20 A
+Bob 102 21 B
+Eve 105 21 B
+Charlie 103 22 C
+SingleLinkedList.Student found
+Updating student Grades
+Alice 101 20 A
+David 104 20 A
+Bob 102 21 B
+Eve 105 21 B
+Charlie 103 22 F
+ */
